4293e095ace3b99f2bc3a8735a29b4aebeb7320d
Henry Co <henryco4388@gmail.com>
2019-12-21 23:25:13 -0800

    header

--- src/components/header/HeaderComponent.js
+++ src/components/header/HeaderComponent.js
@@ -10,56 +10,21 @@ import About from './AboutComponent';
 import Logo from './LogoComponent';
 import Menu from './MenuComponent';
 import Search from './SearchComponent';
+import styles from './header.css';
 
-const styles = StyleSheet.create({
-    header: {
-        height: '40vh',
-        display: 'flex',
-        flexDirection: 'column',
-        position: 'relative',
-    },
-    headerText: {
-        fontFamily: 'Montserrat',
-        fontStyle: 'normal',
-        fontWeight: 'bold',
-        fontSize: '40px',
-        position: 'absolute',
-        top: '30%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        textAlign: 'center',
-    },
-    title: {
-        fontSize: '40px',
-        lineHeight: '98px',
-        letterSpacing: 0.3,
-        color: '#FFFFFF',
-    },
-    subtitle: {
-        fontSize: '19px',
-        lineHeight: '37px',
-        color: '#FFFFFF',
-    },
-    responsiveHeader: {
-        backgroundImage: `${BackgroundImgSmall}`,
-        width: 'auto',
-        maxHeight: '30%'
-    }
-
-});
-
+                    // <a href="e" className='Logo'>Logo</a>
 function HeaderComponent(props) {
     const { logo, title, subtitle, ...otherProps } = props;
     // var {width} = Dimensions.get('window');
     return (
         <Row>
-            <header className={css(styles.header)}>
-                <div className={css(styles.navbar)}>
-                    <Logo/>
-                    <ul >
-                        <About/>
-                        <Menu/>
-                        <Search/>
+            <header className='header'>
+                <div className='navbar'>
+                    <ul className='navlinks'>
+                        <li className='leftnav'><a href="ab">Logo</a></li>
+                        <li className='rightnav'><a href="ab">Home</a></li>
+                        <li className='rightnav'><a href="c">About</a></li>
+                        <li className='rightnav'><a href="d">Blog</a></li>
                     </ul>
                 </div>
                 <img 
@@ -68,18 +33,20 @@ function HeaderComponent(props) {
                         `${BackgroundImgSmall} 300w, ${BackgroundImgMedium} 768w, ${BackgroundImgLarge} 1280w, ${BackgroundImgXLarge} 3200w`
                     }
                 />
-                <Column className={css(styles.headerText)}>
+                <Column className='headerText'>
                     <span 
-                        className={css(styles.title)}
+                        className='title'
                     >{title}</span>
                     <span 
-                        className={css(styles.subtitle)}
+                        className='subtitle'
                     >{subtitle}</span>
                 </Column>  
             </header>
         </Row>
     );
 }
+
+
 
 HeaderComponent.propTypes = {
     title: string
