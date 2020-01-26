009d226e5b64f1dac0e59a1016fe974dd2bca13e
Henry Co <henryco4388@gmail.com>
2020-01-26 15:32:47 -0800

    restarting, setup  blog app as a hybrid model

--- backend/blog_api/api/blog_post/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.BlogPostListView.as_view()),
-    path('view/<slug>/', views.BlogPostDetailView.as_view(), name='post-detail'),
-]
