6595e3c7188706195675716c18bf427d6ea027bc
Henry Co <henryco4388@gmail.com>
2020-01-26 21:24:42 -0800

    importError problem with six, pushing for visibility, fix me

---
+++ backend/danblog/api/post/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from rest_framework import generics 
+
+from .serializers import PostSerializer
+from post.models import Post
+
+class PostListView(generics.ListAPIView):
+    """View For List All Published Posts"""
+
+    queryset = Post.objects.filter(is_published=True)
+    serializer_class = PostSerializer
+    lookup_field = 'slug' 
