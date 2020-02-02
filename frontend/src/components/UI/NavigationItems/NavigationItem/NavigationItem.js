95c8ff3ed102d82da2b9ff3d3c1dc3f70ac72f3a
Henry Co <henryco4388@gmail.com>
2020-02-01 19:58:05 -0800

    ran npm run eject, modified webpack.config.js to load css

--- frontend/src/components/UI/NavigationItems/NavigationItem/NavigationItem.js
+++ frontend/src/components/UI/NavigationItems/NavigationItem/NavigationItem.js
@@ -3,7 +3,6 @@ import React from "react";
 import cssClass from "./NavigationItem.css";
 
 const navigationItem = props => {
-    // return <li className={cssClass.Item}>{props.children}</li>;
     let classes = [cssClass.Item,]
     if (props.active) {
         classes.push(cssClass.active)
