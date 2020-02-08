84a468967d120bebad0b6a84b5516abab036da80
Henry Co <henryco4388@gmail.com>
2020-02-07 19:58:18 -0800

    added hamburger sidebar

--- frontend/src/hoc/Layout/Layout.js
+++ frontend/src/hoc/Layout/Layout.js
@@ -2,17 +2,28 @@ import React, { Component } from "react";
 
 import Aux from "../Aux/Aux";
 import Toobar from "../../components/UI/Toolbar/Toolbar";
-// import Sidebar from "../../components/UI/Sidebar/Sidebar";
+import Sidebar from "../../components/UI/Sidebar/Sidebar";
 import Logo from "../../components/UI/Logo/Logo";
 import cssClass from "./Layout.css";
 
 class Layout extends Component {
+    state = {
+        isSidebarOpen: false
+    };
+
+    SidebarToggleHandler = () =>
+        this.setState(prevState => {
+            return {
+                isSidebarOpen: !prevState.isSidebarOpen
+            };
+        });
+        
     render() {
         return (
             <Aux>
                 <Logo />
-                <Toobar />
-                
+                <Toobar clicked={this.SidebarToggleHandler} />
+                <Sidebar isSidebarOpen={this.state.isSidebarOpen} />
                 <main>{this.props.children}</main>
             </Aux>
         )
