6efde7610daaaf10e9ea79391e9e8d934b5fba2d
Henry Co <henryco4388@gmail.com>
2020-02-08 21:00:58 -0800

    added post body, fixed console bug

--- frontend/src/components/Posts/Posts.js
+++ frontend/src/components/Posts/Posts.js
@@ -5,7 +5,7 @@ import Post from "./Post/Post";
 const posts = props => {
     const postList = props.postList.map(post => (
         <Post
-            key={post.id}
+            key={post.slug}
             title={post.title}
             datePublished={post.published_on}
             totalComments={post.total_comments}
