1b458334c5401264603b685b796e23cc44c0b4e7
Henry Co <henryco4388@gmail.com>
2020-02-18 19:52:19 -0800

    created button componenet

---
+++ frontend/src/components/UI/Button/Button.js
@@ -0,0 +1,10 @@
+
+import React from "react";
+
+import cssClass from "./Button.css";
+
+const button = props => {
+    return <button className={cssClass.Button}>{props.children}</button>;
+};
+
+export default button;
