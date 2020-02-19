141cff7a5a4d7d771a24ca6e02166a0b4bacebe2
Henry Co <henryco4388@gmail.com>
2020-02-18 20:36:50 -0800

    server now stores a token if a login is successful and added a loading spinner to login

--- frontend/src/store/actions/auth.js
+++ frontend/src/store/actions/auth.js
@@ -1,7 +1,34 @@
 import * as actionsTypes from "./actionTypes";
+import AxiosInstance from "../../AxiosInstance";
 
 export const authLoginInit = () => {
     return {
         type: actionsTypes.AUTH_LOGIN_INIT
+    };
+};
+
+export const authLoginSuccess = data => {
+    return {
+        type: actionsTypes.AUTH_LOGIN_SUCCESS,
+        token: data.token
+    };
+};
+
+export const authLoginFail = error => {
+    return {
+        type: actionsTypes.AUTH_LOGIN_FAIL
+    };
+};
+
+export const authLogin = loginCredentials => {
+    return dispatch => {
+        dispatch(authLoginInit());
+        AxiosInstance.post("auth/login/", loginCredentials)
+            .then(response => {
+                dispatch(authLoginSuccess(response.data));
+            })
+            .catch(error => {
+                dispatch(authLoginFail(error));
+            });
     };
 };
